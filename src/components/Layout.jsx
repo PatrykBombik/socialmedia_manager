1f59024948cb95e75082df1afc41e61742eb758c
PatrykBombik <patryk.bombik@gmail.com>
2023-06-07 23:39:38 +0200

    footer layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+ import { Outlet } from "react-router-dom";
 import ResponsiveAppBar from "./navbar.jsx";
+ import Footer from "./landingpage/footer.jsx";
 
 function Layout() {
     return (
         <>
             <ResponsiveAppBar />
             <Outlet />
+            <Footer/>
         </>
     );
 }
---
+++ src/components/landingpage/footer.jsx
@@ -0,0 +1,27 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+
+export default function Footer() {
+    return (
+        <Container maxWidth="xl" sx={{
+            backgroundColor: "lightgray",
+            marginTop: "5%",
+            textDecoration: "none"
+
+        }}>
+        <Grid container spacing={2} >
+            <Grid item md={12} sm={12} xs={12}>
+                <Button href="/">Jak zacząć?</Button>
+                <Button href="/daily">Daily</Button>
+                <Button href="/">Zarejestruj się</Button>
+                <Button href="/login">Logowanie</Button>
+            </Grid>
+            <Grid item md={12} sm={12} xs={12}>
+                <p>SocialMediaManager</p>
+                <p>© 2023 Your Company.  All rights reserved.</p>
+            </Grid>
+        </Grid>
+        </Container>
+    )
+}
