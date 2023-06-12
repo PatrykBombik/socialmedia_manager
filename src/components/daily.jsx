366db616194e5d8f0b274e0be2420bdbcb13c07e
PatrykBombik <patryk.bombik@gmail.com>
2023-06-12 17:50:10 +0200

    showing tasks

--- src/components/daily.jsx
+++ src/components/daily.jsx
@@ -4,62 +4,67 @@ import {getAllTasks, sendTaskData} from "../helpers/api.jsx";
 import Button from "@mui/material/Button";
 
 export default function Daily() {
-    const [taskTwitter, setTaskTwitter] = useState([]);
+    const [tasksTwitter, setTasksTwitter] = useState([]);
     const [titleTwitter, setTittleTwitter] = useState('');
     const [descTwitter, setDescTwitter] = useState('');
 
     useEffect(() => {
         getAllTasks("Twitter")
             .then((data) => {
-                setTaskTwitter(data)
+                setTasksTwitter(data)
             })
             .catch(console.error)
     }, [])
-
-    const [taskReddit, setTaskReddit] = useState([]);
-    const [titleReddit, setTittleReddit] = useState('');
-    const [descReddit, setDescReddit] = useState('');
-
-    useEffect(() => {
-        getAllTasks("Reddit")
-            .then((data) => {
-                setTaskReddit(data)
-            })
-            .catch(console.error)
-    }, [])
-
-    const [taskInstagram, setTaskInstagram] = useState([]);
-    const [titleInstagram, setTittleInstagram] = useState('');
-    const [descInstagram, setDescInstagram] = useState('');
-
-    useEffect(() => {
-        getAllTasks("Instagram")
-            .then((data) => {
-                setTaskInstagram(data)
-            })
-            .catch(console.error)
-    }, [])
-
-    const [taskSnapchat, setTaskSnapchat] = useState([]);
-    const [titleSnapchat, setTittleSnapchat] = useState('');
-    const [descSnapchat, setDescSnapchat] = useState('');
-
-    useEffect(() => {
-        getAllTasks("Snapchat")
-            .then((data) => {
-                setTaskSnapchat(data)
-            })
-            .catch(console.error)
-    }, [])
-
 
     async function handleTwitterSubmit(event) {
         event.preventDefault();
         const result = await sendTaskData("Twitter", {
             title: titleTwitter, description: descTwitter, status: 'open', addedDate: new Date()
         });
-        console.log(result)
+
+        setTittleTwitter('');
+        setDescTwitter('');
+        setTasksTwitter([...tasksTwitter, result])
     }
+
+    const [tasksReddit, setTasksReddit] = useState([]);
+    const [titleReddit, setTittleReddit] = useState('');
+    const [descReddit, setDescReddit] = useState('');
+
+    useEffect(() => {
+        getAllTasks("Reddit")
+            .then((data) => {
+                setTasksReddit(data)
+            })
+            .catch(console.error)
+    }, [])
+
+    const [tasksInstagram, setTasksInstagram] = useState([]);
+    const [titleInstagram, setTittleInstagram] = useState('');
+    const [descInstagram, setDescInstagram] = useState('');
+
+    useEffect(() => {
+        getAllTasks("Instagram")
+            .then((data) => {
+                setTasksInstagram(data)
+            })
+            .catch(console.error)
+    }, [])
+
+    const [tasksSnapchat, setTasksSnapchat] = useState([]);
+    const [titleSnapchat, setTittleSnapchat] = useState('');
+    const [descSnapchat, setDescSnapchat] = useState('');
+
+    useEffect(() => {
+        getAllTasks("Snapchat")
+            .then((data) => {
+                setTasksSnapchat(data)
+            })
+            .catch(console.error)
+    }, [])
+
+
+
 
     return (
         <>
@@ -88,6 +93,16 @@ export default function Daily() {
                         </div>
                         <Button variant="contained" onClick={handleTwitterSubmit}>Add</Button>
                     </form>
+                    <section>
+                        {tasksTwitter.map((task) => (
+                            <div key={task.id}>
+                                <strong>{task.title}</strong> <span> - {task.description}</span>
+                                <button>Add operation</button>
+                                <button>Finish</button>
+                                <button>Delete</button>
+                            </div>
+                        ))}
+                    </section>
                 </Grid>
                 <Grid item md={12} sm={12} xs={12}>
                     <h1>Reddit</h1>
